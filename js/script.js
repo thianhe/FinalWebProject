79511da930809965a3a24e2c7683e5d6d6316c95
jykuo <thianhe@hotmail.com>
2018-06-20 01:03:27 +0800

    clear something

--- js/script.js
+++ js/script.js
@@ -6,4 +6,19 @@ $(document).ready(function() {
     $select1.on('change', function() {
         $select2.html($options.filter('[value="' + this.value + '"]'));
     }).trigger('change');
+
+    var countries = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        // url points to a json file that contains an array of country names, see
+        // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
+        prefetch: '../data/countries.json'
+    });
+
+    // passing in `null` for the `options` arguments will result in the default
+    // options being used
+    $('.typeahead').typeahead(null, {
+        name: 'countries',
+        source: countries
+    });
 });
