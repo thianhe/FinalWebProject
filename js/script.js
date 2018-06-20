46c7ad7a255ce4cae0ffec138a97e04baca1c398
jykuo <thianhe@hotmail.com>
2018-06-21 02:34:40 +0800

    added sample.html

--- js/script.js
+++ js/script.js
@@ -25,12 +25,24 @@ $(document).ready(function() {
         }
     });
 
-    $('li a').on("click", function() {
-        var target = $(this).attr('href')
-        var offset = $(target).offset().top
-        $('body').animate({
-            scrollTop: offset
-        }, '800')
+    $('#carouselExample').on('slide.bs.carousel', function(e) {
+
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 4;
+        var totalItems = $('.carousel-item').length;
+
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+            var it = itemsPerSlide - (totalItems - idx);
+            for (var i = 0; i < it; i++) {
+                // append slides to end
+                if (e.direction == "left") {
+                    $('.carousel-item').eq(i).appendTo('.carousel-inner');
+                } else if (e.direction == "right") {
+                    $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                }
+            }
+        }
     });
 
 
