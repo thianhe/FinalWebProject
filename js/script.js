7630ae46f0b9a7321940d5939f282c5f53bc19a7
jykuo <thianhe@hotmail.com>
2018-06-20 21:31:58 +0800

    added search engine

--- js/script.js
+++ js/script.js
@@ -7,18 +7,13 @@ $(document).ready(function() {
         $select2.html($options.filter('[value="' + this.value + '"]'));
     }).trigger('change');
 
-    var countries = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.whitespace,
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        // url points to a json file that contains an array of country names, see
-        // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
-        prefetch: '../data/countries.json'
+    $('li a').on("click", function() {
+        var target = $(this).attr('href')
+        var offset = $(target).offset().top
+        $('body').animate({
+            scrollTop: offset
+        }, '800')
     });
 
-    // passing in `null` for the `options` arguments will result in the default
-    // options being used
-    $('.typeahead').typeahead(null, {
-        name: 'countries',
-        source: countries
-    });
+
 });
