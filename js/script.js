804481a1679d5253f935bafd9ce614aff5d6fb19
jykuo <thianhe@hotmail.com>
2018-06-23 12:51:44 +0800

    minor changes

--- js/script.js
+++ js/script.js
@@ -28,5 +28,84 @@ $(document).ready(function() {
         }
     });
 
+    /*firebase*/
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDHLiFLoabQKjEizi0s6OSlpvrqA3iMBGI",
+        authDomain: "test01-15f7d.firebaseapp.com",
+        databaseURL: "https://test01-15f7d.firebaseio.com",
+        projectId: "test01-15f7d",
+        storageBucket: "test01-15f7d.appspot.com",
+        messagingSenderId: "487451576900"
+    };
+    firebase.initializeApp(config);
 
+    var firebase = require('firebase');
+    var firebaseui = require('firebaseui');
+
+    ui.start('#firebaseui-auth-container', {
+        signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+    });
+
+    ui.start('#firebaseui-auth-container', {
+        signInOptions: [{
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            requireDisplayName: false
+        }]
+    });
+
+    ui.start('#firebaseui-auth-container', {
+        signInOptions: [
+            // List of OAuth providers supported.
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ],
+    });
+    ui.start('#firebaseui-auth-container', {
+        signInOptions = [{
+            provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            scopes: [
+                'https://www.googleapis.com/auth/plus.login'
+            ],
+            customParameters: {
+                // Forces account selection even when one account
+                // is available.
+                prompt: 'select_account'
+            }
+        }, ]
+    });
+
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    var uiConfig = {
+        callbacks: {
+            signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+                // User successfully signed in.
+                // Return type determines whether we continue the redirect automatically
+                // or whether we leave that to developer to handle.
+                return true;
+            },
+            uiShown: function() {
+                // The widget is rendered.
+                // Hide the loader.
+                document.getElementById('loader').style.display = 'none';
+            }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: '<url-to-redirect-to-on-success>',
+        signInOptions: [
+            // Leave the lines as is for the providers you want to offer your users.
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: '<your-tos-url>'
+    };
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
 });
